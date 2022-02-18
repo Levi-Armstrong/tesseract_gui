bca7ad315f08493f6d070fca573bfc126df8121f
Levi Armstrong <levi.armstrong@gmail.com>
2022-02-17 22:00:03 -0600

    Add imgui

---
+++ 3rdparty/imgui/include/imgui/imgui_demo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace ImGui
+{
+// Demonstrate most Dear ImGui features (this is big function!)
+// You may execute this function to experiment with the UI and understand what it does.
+// You may then search for keywords in the code when you are interested by a specific feature.
+void ShowDemoWindow(bool* p_open);
+
+}
---
+++ 3rdparty/imgui/include/imgui/imgui_themes.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace ImGui
+{
+void ApplyCorporateGreyTheme(bool use_3d = false);
+}
